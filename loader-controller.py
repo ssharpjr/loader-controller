9b8a1519669cd99395ee9ab6647bbd2a7a566a94
Stacey Sharp <ssharpjr@gmail.com>
2016-05-02 16:59:04 -0400

    added LCD functions

--- loader-controller.py
+++ loader-controller.py
@@ -1,24 +1,82 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# TODO: Setup LCD
 # TODO: Setup Loader Controller
 
 
 import sys
 import requests
+import time
 import json
 
+import Adafruit_CharLCD as LCD
+import Adafruit_GPIO.MCP230xx as MCP
+import RPi.GPIO as io  # For standard GPIO methods.
 
 # CONSTANTS
 PRESS_ID = '125'  # Should be 125 for test.  This does not change!
 
 # Variables
+DEBUG = True
 api_url = 'http://localhost:5000'
 
 
+###############################################################################
+# Setup the LCD and MCP.
+###############################################################################
+# Define the MCP pins connected to the LCD.
+lcd_rs = 0
+lcd_en = 1
+lcd_d4 = 2
+lcd_d5 = 3
+lcd_d6 = 4
+lcd_d7 = 5
+lcd_red = 6
+lcd_green = 7
+lcd_blue = 8
+
+# Define LCD column and row size for a 20x4 LCD.
+lcd_columns = 20
+lcd_rows = 4
+
+# Initialize MCP23017 device using its default 0x20 I2C address.
+gpio = MCP.MCP23017()
+
+# Initialize the LCD using the pins.
+lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                              lcd_columns, lcd_rows, lcd_red, lcd_green,
+                              lcd_blue, gpio=gpio)
+###############################################################################
+
+
+def lcd_ctrl(msg, color, clear=True):
+    # Send instructions to the LCD.
+    # Colors are Red, Green, Blue values.
+    # all zeros equals off, all ones equals white
+    color = color
+    if clear:
+        lcd.clear()
+    if color == 'red':
+        lcd.set_color(1.0, 0.0, 0.0)  # Red
+    elif color == 'green':
+        lcd.set_color(0.0, 1.0, 0.0)  # Green
+    elif color == 'blue':
+        lcd.set_color(0.0, 0.0, 1.0)  # Blue
+    elif color == 'white':
+        lcd.set_color(1.0, 1.0, 1.0)  # White
+    elif color == 'off':
+        lcd.set_color(0.0, 0.0, 0.0)  # Off
+    else:
+        lcd.set_color(0.0, 0.0, 0.0)  # Off
+    lcd.message(msg)
+
+
 def get_wo_scan():
-    wo_scan = input("Scan Workorder: ")
+    lcd_ctrl("SCAN\nWORKORDER\nNUMBER", white)
+    if DEBUG:
+        wo_scan = input("Scan Workorder: ")
+    else:
+        wo_scan = input()  # No console output
     # wo_scan = '9934386'  # Should be 9934386 for test.
     return wo_scan
 
@@ -29,8 +87,11 @@ def wo_api_request(wo_id):
     data = json.loads(resp.text)
     try:
         if data['error']:
+            lcd_ctrl("INVALID WORKORDER!", red)
+            if DEBUG:
                 print("Invalid Workorder!")
-                run_or_exit_program('run')
+            sleep(5)  # Pause so the user can read the error.
+            run_or_exit_program('run')
     except:
         pass
     try:
@@ -47,7 +108,10 @@ def serial_api_request(sn):
     data = json.loads(resp.text)
     try:
         if data['error']:
-            print("Invalid Serial Number!")
+            lcd_ctrl("INVALID SERIAL\nNUMBER!", red)
+            if DEBUG:
+                print("Invalid Serial Number!")
+            sleep(5)  # Pause so the user can read the error.
             run_or_exit_program('run')
     except:
         pass
@@ -56,35 +120,50 @@ def serial_api_request(sn):
 
 
 def get_rmat_scan():
-    rmat_scan = str(input("Scan Raw Material Serial Number: "))
+    lcd_ctrl("SCAN\nRAW MATERIAL\nSERIAL NUMBER", white)
+    if DEBUG:
+        rmat_scan = str(input("Scan Raw Material Serial Number: "))
+    else:
+        rmat_scan = str(input())
     # rmat_scan = 'S07234585' for test.
     if not rmat_scan.startswith('S'):
-        print("Not a Serial Number!")
+        lcd_ctrl("NOT A VALID\nSERIAL NUMBER!", red)
+        if DEBUG:
+            print("Not a Serial Number!")
+        sleep(5)  # Pause so the user can read the error.
         run_or_exit_program('run')
     rmat_scan = rmat_scan[1:]  # Strip off the "S" Qualifier.
     return rmat_scan
 
 
 def start_loader():
-    # GPIO control for PST2 and CT
+    # TODO
+    # GPIO control for PST.
     input("Loader is running.  Press ENTER to exit test.")
     run_or_exit_program('exit')
 
 
 def run_or_exit_program(status):
     if status == 'run':
-        print("Starting over...")
+        if DEBUG:
+            print("Starting over...")
         run()
     elif status == 'exit':
-        print("Exiting")
+        if DEBUG:
+            print("Exiting")
         sys.exit()
 
 
-#####################################################################
-
+###############################################################################
+# Main
+###############################################################################
 
 def main():
-    print("This is Press: " + PRESS_ID)
+    lcd_msg ="LOADER\nCONTROLLER\nASSIGNED TO\nPRESS" + PRESS_ID
+    lcd_ctrl(lcd_msg, white)
+    if DEBUG:
+        print("This is Press: " + PRESS_ID)
+    sleep(3)
     # Scan the Workorder Number (ID) Barcode.
     wo_id_from_wo = get_wo_scan()
 
@@ -92,14 +171,19 @@ def main():
     press_from_wo, rmat_from_wo = wo_api_request(wo_id_from_wo)
 
     # Verify the Press Number.
-    print("Checking if workorder is currently running on this press...")
+    if DEBUG:
+        print("Checking if workorder is currently running on this press...")
     if press_from_wo == PRESS_ID:
-        print("Match.  Workorder #" + wo_id_from_wo +
-              " is running on Press #" + PRESS_ID)
-        print("Good Workorder.  Continuing...")
+        if DEBUG:
+            print("Match.  Workorder #" + wo_id_from_wo +
+                  " is running on Press #" + PRESS_ID)
+            print("Good Workorder.  Continuing...")
     else:
-        print("Incorrect Workorder!")
-        print("This Workorder is for press: " + press_from_wo)
+        lcd_ctrl("INCORRECT\nWORKORDER!", red)
+        if DEBUG:
+            print("Incorrect Workorder!")
+            print("This Workorder is for press: " + press_from_wo)
+        sleep(5)  # Pause so the user can see the error.
         run_or_exit_program('run')
 
     # Scan the Raw Material Serial Number Barcode.
@@ -109,10 +193,15 @@ def main():
     rmat_from_api = serial_api_request(serial_from_label)
 
     # Verify the Raw Material Item Number.
-    print("Checking if raw material matches this workorder...")
+    if DEBUG:
+        print("Checking if raw material matches this workorder...")
     if rmat_from_wo == rmat_from_api:
-        print("Material matches workorder.  Continuing...")
-        print("Starting the Loader!")
+        if DEBUG:
+            print("Material matches workorder.  Continuing...")
+            print("Starting the Loader!")
+        # Display Press ID, FG Item Description and RM Description?
+        lcd_msg = "Press #" + PRESS_ID
+        lcd_ctrl(lcd_msg, green)
         start_loader()
     else:
         print("Invalid Material!")
