7add54eca022629976f8f191f6e6f9af6b915c5d
Stacey Sharp <ssharpjr@gmail.com>
2016-05-18 21:04:09 -0400

    added irtest

---
+++ examples/irtest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+from time import sleep
+import RPi.GPIO as io
+
+io.setmode(io.BCM)
+
+io.setup(18, io.IN, pull_up_down=io.PUD_UP)
+
+
+print("Checking IR Sensor")
+while True:
+    try:
+        if io.input(18) == 1:
+            print("IR Detected")
+            sleep(1)
+        elif io.input(18) == 0:
+            print("IR Not Detected")
+            sleep(1)
+    except KeyboardInterrupt:
+        io.cleanup()
+        sys.exit()
